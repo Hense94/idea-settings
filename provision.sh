53161504e6155375eb38069f2438d52bdf4bc4a0
Henrik <hhs19942@gmail.com>
2019-05-02 22:43:22 +0200

    Complete overhaul

--- provision.sh
+++ provision.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #Create symlinks
-input="symlinks"
+echo "Creating symlinks..."
 
 while IFS= read -r var
 do
@@ -12,34 +12,107 @@ do
   #Create dst dir if not present
   [ -d $DSTDIR ] || mkdir -p $DSTDIR 
   cmp --silent "$src" "$dst" || ln -s "$src" "$dst"
-done < "$input"
+done < "symlinks"
+echo "done!"
 
-exit 0
+echo ""
+#Install curl
+echo "Installing curl..."
+sudo apt install curl -y
+echo "done!"
 
-#Run apt installs (required because of curl)
-./apt_installs.sh
+echo ""
+#Add Albert repo
+echo "Adding albert repo..."
+DIST_VERSION=$(lsb_release -a 2> /dev/null | grep Release | grep -Po "[0-9\.]+$")
+#sudo apt install apt-transport-https -y
+curl https://build.opensuse.org/projects/home:manuelschneid3r/public_key | sudo apt-key add -
+sudo sh -c "echo 'deb http://download.opensuse.org/repositories/home:/manuelschneid3r/xUbuntu_$DIST_VERSION/ /' > /etc/apt/sources.list.d/home:manuelschneid3r.list"
+echo "done!"
 
+echo ""
+#Add Sublime repo
+echo "Adding Sublime repo..."
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+echo "done!"
+
+echo ""
+#Run apt installs
+echo "Running apt installs and removes..."
+sudo apt update -y
+while IFS= read -r APT_LINE
+do
+  IFS=':' read -r  -a APT <<< "$APT_LINE"
+  eval ACTION="${APT[0]}"
+  eval PKG="${APT[1]}"
+  PKG_INSTALLED=$(dpkg -s $PKG 2> /dev/null | grep "install ok installed")
+  if [ "" == "$PKG_INSTALLED" ]; then
+    if [ "i" == "$ACTION" ]; then
+      echo "Installing $PKG..."
+      sudo apt install "$PKG" -y
+      echo "done!"
+    else
+      echo "Removing $PKG..."
+      sudo apt purge "$PKG" -y
+      echo "done!"
+    fi
+  else
+    if [ "i" == "$ACTION" ]; then
+      echo "$PKG already installed"
+    else
+      echo "$PKG already removed"
+    fi
+  fi
+done < "apt_packages"
+echo "done!"
+
+echo ""
 #NVM and node
+echo "Installing NVM..."
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
-nvm install node
+echo "done!"
 
+echo ""
 #Chrome
-curl -L -O "https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"
-sudo dpkg --install google-chrome-stable_current_amd64.deb
-rm google-chrome-stable_current_amd64.deb
+CHROME_INSTALLED=$(dpkg -s "google-chrome-stable" 2> /dev/null | grep "install ok installed")
+if [ "" == "$CHROME_INSTALLED" ]; then
+  echo "Installing Chrome..."
+  curl -L -O "https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"
+  sudo dpkg --install google-chrome-stable_current_amd64.deb -y
+  rm google-chrome-stable_current_amd64.deb
+else 
+  echo "Chrome already installed"
+fi
+echo "done!"
 
+echo ""
 #Monaco font
-curl -L -O "http://www.gringod.com/wp-upload/software/Fonts/Monaco_Linux.ttf"
-mv Monaco_Linux.ttf ~/.local/share/fonts/Monaco_Linux.ttf
-fc-cache -f
+if [ -f ~/.local/share/fonts/Monaco_Linux.ttf ]; then
+  echo "Monaco (font) already installed"
+else
+  echo "Installing Monaco (font)..."
+  curl -L -O "http://www.gringod.com/wp-upload/software/Fonts/Monaco_Linux.ttf"
+  [ -d ~/.local/share/fonts ] || mkdir -p ~/.local/share/fonts
+  mv Monaco_Linux.ttf ~/.local/share/fonts/Monaco_Linux.ttf
+  fc-cache -f
+fi
+echo "done!"
 
+echo ""
 #Use numix circle and monaco font
+echo "Setting icon theme and font..."
 dconf write /org/gnome/desktop/interface/icon-theme "'Numix-Circle'"
 dconf write /org/gnome/desktop/interface/monospace-font-name= "'Monaco 13'"
+echo "done!"
 
+echo ""
 #Keyboard shortcuts
+echo "Setting keyboard shortcuts..."
 dconf load /org/gnome/settings-daemon/plugins/media-keys/ < ~/dotfiles/keys.conf
+echo "done!"
 
+echo ""
 #Terminal profile
 dlist_append() {
     local key="$1"; shift
@@ -58,6 +131,11 @@ dlist_append() {
 TERMINAL_PATH=/org/gnome/terminal/legacy/profiles:
 PROFILE_SLUG=3d15fe59-69d5-40c3-be2a-6da1ed714a55
 
+echo "Setting terminal profile"
 dconf load $TERMINAL_PATH/ < ~/dotfiles/terminal.conf
 dlist_append $TERMINAL_PATH/list "$PROFILE_SLUG"
+echo "done!"
 
+#Update all packages and clean up 
+sudo apt-get update -y && sudo apt-get upgrade -y
+sudo apt autoremove -y
